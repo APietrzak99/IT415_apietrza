53486cd5f7fa2725d94bbd3ae84f4fe2a9fa7aa4
Anthony Pietrzak <apietrza@mail.walshcollege.edu>
2021-02-20 16:46:24 -0500

    init commit

--- home_app/App.js
+++ home_app/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-const YourApp = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>
-        Try editing me! 🎉
-      </Text>
-    </View>
-  );
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+      <View style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center"
+        }}>
+        <Text>Hello, world!</Text>
+      </View>
+    );
+  }
 }
-
-export default YourApp;
