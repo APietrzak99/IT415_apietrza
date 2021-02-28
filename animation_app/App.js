58120069d79650d1df3db7911ca460d8fcd31ea7
Anthony Pietrzak <apietrza@mail.walshcollege.edu>
2021-02-28 13:47:52 -0500

    app 8 100% done

--- animation_app/App.js
+++ animation_app/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import { Image, Animated, Text, View, StyleSheet, ImageBackground, Dimensions,Button } from 'react-native';
+import { Image, Animated, View, StyleSheet, ImageBackground, Dimensions, Button} from 'react-native';
 
 export default class AnimationApp extends Component {
 
   state = {
-    fadeAnim: new Animated.Value(0)
+    fadeAnim: new Animated.Value(0),
+    spinAnim: new Animated.Value(0),
+    shakeAnimation: new Animated.Value(0)
   };
+
+  startShake = () => {
+    Animated.sequence([
+      Animated.timing(this.state.shakeAnimation, { toValue: 10, duration: 100, useNativeDriver: true }),
+      Animated.timing(this.state.shakeAnimation, { toValue: -10, duration: 100, useNativeDriver: true }),
+      Animated.timing(this.state.shakeAnimation,{ toValue: 10, duration: 100, useNativeDriver: true }),
+      Animated.timing(this.state.shakeAnimation, { toValue: 0, duration: 100, useNativeDriver: true })
+    ]).start();
+ }
 
   fadeIn = () => {
     Animated.timing(this.state.fadeAnim, {
@@ -23,8 +34,24 @@ export default class AnimationApp extends Component {
     }).start();
   };
   
+  spinning = () => {
+    this.state.spinAnim.setValue(0);
+    Animated.timing(this.state.spinAnim, {
+      toValue: 1,
+      duration: 3000,
+      useNativeDriver: false
+    }).start();
+  };
 
   render() {
+    const spin = this.state.spinAnim.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    })
+    const shake = this.state.shakeAnimation.interpolate({
+      inputRange: [-1, 1],
+      outputRange: [-0.5,0.5]
+    })
     return (
           <View style={styles.container}>
         <ImageBackground source={require('./assets/spiderman_scene.png')} style={styles.backgroundImage}>
@@ -38,12 +65,16 @@ export default class AnimationApp extends Component {
         >
           <Image source={require('./assets/spiderman.png')} style={styles.SpidermanImage}/>
         </Animated.View>
+        <Animated.View style={{ transform: [{translateX: shake}] }}>  
+        <Animated.View style={{transform: [{rotate: spin}] }} >
         <Image source={require('./assets/spiderman_mask.png')} style={styles.Image}/>
+        </Animated.View>
+        </Animated.View>
         <View style={styles.buttonRow}>
         <Button title="Fade In" onPress={this.fadeIn} />
           <Button title="Fade Out" onPress={this.fadeOut} />
-          <Button title="Wiggle" onPress={this.fadeIn} />
-          <Button title="     ?     " onPress={this.fadeOut} />
+          <Button title="Wiggle" onPress={this.startShake} />
+          <Button title="    ?     " onPress={this.spinning} />
           </View>
         </ImageBackground>
         </View>
