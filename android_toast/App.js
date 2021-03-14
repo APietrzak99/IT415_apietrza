4a6bc32071bea9409f9c3034d6abb62dcc92a72c
Anthony Pietrzak <apietrza@mail.walshcollege.edu>
2021-03-14 16:11:37 -0400

    week 10 nm assignment done

---
+++ android_toast/App.js
@@ -0,0 +1,67 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  Touchable,
+  ToastAndroid,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const showToast = () => {
+    ToastAndroid.show("A random toast appeared!", ToastAndroid.SHORT);
+  };
+
+  return (
+    <View style={styles.container}>
+        <View style={styles.countContainer}>
+        </View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => showToast()}
+        >
+          <Text>Press Here</Text>
+        </TouchableOpacity>
+      </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 10
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  },
+  countContainer: {
+    alignItems: "center",
+    padding: 10
+  }
+});
+
+export default App;
